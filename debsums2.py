d4e46efa1adfe4eee05206a2e68a94dde1b04c0c
reox <hello@reox.at>
2020-05-07 18:27:54 +0200

    Update debsums2.py
    Adding licence text to debsums2

--- debsums2.py
+++ debsums2.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python
 # -*- coding: latin-1 -*-
-
+#
+# debsums2 - dpkg integrity check
+# Copyright (C) 2014  Roland Wenzel
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 __date__    = '2014-10-10'
 __version__ = 2.0
 
